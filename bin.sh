d7d5e3bddeb73e1ef5c2d7c27075903fc580d769
Subodh <subodhss23@gmail.com>
2019-11-06 09:48:07 -0800

    content lost

--- bin.sh
+++ bin.sh
@@ -40,5 +40,48 @@
   --> shows the difference between two files
 
 
-******************NETWORKING******************
+******************NETWORKING*****************
+
+11. ssh
+
+
+12. ifconfig
+	--> configure network interface parameters
+	-> ifconfig | grep inet
+
+13. ping
+	--> checking if the host is sending/receiving data
+
+13. telnet
 	
+
+14. curl
+	--> tool to transfer data from or to a server
+ 
+15. touch
+	--> create a file
+	--> update a timestamp if file already exist
+
+15. scp
+	--> securly copy files
+
+16. sftp
+	--> secure file transfer program  
+
+16. rsync
+	--> allows rsync to transfer just the differences between two sets of files across the n		network connection.
+	--> rsync copies files either to or from a remote host
+
+
+*******************PRODUCTIVITY TOOLS******************
+
+17. cal
+	--> opens calender
+
+18. bc
+	--> opens calculator
+	-> do scale=3 to get three float point after decimal sign
+
+***awk, sed,*************
+
+******	
