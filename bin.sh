506cfb2f23e61ba204614678b96cdbfc0189e948
Subodh <subodhss23@gmail.com>
2019-11-06 02:10:13 -0800

    command line rocks

---
+++ bin.sh
@@ -0,0 +1,24 @@
+1. details about the code 
+	man [command]
+		alternate
+	help [command]
+	[command] --h
+
+2. | pipe sign
+  --> feed the output of first to input of  second command
+
+3. TAB
+  --> auto complete
+
+4. cd 
+  --> home directory
+  cd -
+  --> back to previous location
+
+5. find
+  --> find file or directory
+  find . --> shows all files in directory
+  find . -name '*.js' --> shows all files with .js extension
+  find . -name '*.js' -size +500k -> shows all files with .js extension with size above 500k
+  	
+
