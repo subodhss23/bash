97d21b3a8f9f890e86ff7d533f1859402e7dbc49
Subodh <subodhss23@gmail.com>
2019-11-07 10:56:58 -0800

    ownership and -R for recursive

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -4,4 +4,32 @@
 
 2) ssh --> openSSH SSH client (remote login program)
 
+3) whoami --> current user
 
+4) tty --> console name
+
+5) who --> list all user
+
+6) which --> path of certain command
+
+7) locate --> maintains the list of data base
+
+8) pwd --> present working directory
+
+9) cal --> to view the calender
+
+10) date --> to view the date 
+	--> date -r [filename] -> to view when it was last modified
+	--> date +%T -> current time
+	--> date + %D -> full date
+
+11) chmod --> change the permission
+	--> 4 - read, 2 - write, 1 - execute
+	--> chmod -R --> to give permission to all the files inside the folder
+
+11)chgrp [group-name]--> change group 
+
+12) chown --> change the ownership of file or folder
+		chown -R --> changing all nested files and folders
+
+13
