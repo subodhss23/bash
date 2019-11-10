5ee6c90ce8330ca6569f311e02d71eabeb46219d
subodhss23 <subodhss23@gmail.com>
2019-11-10 09:12:17 -0800

    c++, let's get it

--- first.cpp
+++ first.cpp
@@ -1,8 +1,27 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int	main(void){
-	cout << "Hello World!";
-	
-	return 0;
+
+int     main(void){
+
+    cout << "What is happening bro" <<endl;
+
+    int x;
+    cout << "Enter a number to store" <<endl;
+    cin >> x;
+    cout << "The number you have entered is " << x << endl;
+
+    char c ;
+    cout << "Enter a character" << endl;
+    cin >> c;
+    cout << "The character you entered is " << c <<endl;
+
+    float f;
+    cout << "Etner a float" << endl;
+    cin >> f;
+    cout << "The float you entered is " << f;
+
+
+    return 0;
 }
