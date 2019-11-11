894f9d81729ac6fca0ae19a8d2af2e33a9ea670a
subodhss23 <subodhss23@gmail.com>
2019-11-10 20:19:49 -0800

    cpp

--- first.cpp
+++ first.cpp
@@ -4,24 +4,23 @@ using namespace std;
 
 
 int     main(void){
-
-    cout << "What is happening bro" <<endl;
-
+    
     int x;
-    cout << "Enter a number to store" <<endl;
+
+    cout << "Enter a number" << endl;
     cin >> x;
-    cout << "The number you have entered is " << x << endl;
 
-    char c ;
-    cout << "Enter a character" << endl;
-    cin >> c;
-    cout << "The character you entered is " << c <<endl;
-
-    float f;
-    cout << "Etner a float" << endl;
-    cin >> f;
-    cout << "The float you entered is " << f;
-
+    int y;
+    cout << "Enter another number" << endl;
+    cin >> y;
+    
+    if (x > y){
+        cout << "X is greater than Y";
+    } else if (x < y){
+        cout << "Y is greater than X";
+    } else {
+        cout << "not a valid operation";
+    }
 
     return 0;
 }
